4b68f046ab52a5e6c9aa5518aa8350b7a3b23164
Igor Ostroverkhov <muravey73@mail.ru>
2022-08-30 23:59:58 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 27_536; // стоимость билета
+
+        int mile = 20; // стоимость одной бонусной мили
+        int bonusMiles = ticket/mile; // количество бонусных миль
+
+        System.out.println(bonusMiles);
+         }
+}
